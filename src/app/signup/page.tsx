e2d61a9e891e89c8a870817911c4d31309ebb097
yukiuyeda <yueda940408@gmail.com>
2025-02-20 01:08:17 +0900

    認証機能実装途中

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,42 @@
 import { supabase } from "@/utils/supabase";
 import { useState } from "react";
 
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
 const page = () => {
   return (
     <div>
-      <form></form>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="name@company.com"
+            value={email}
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="●●●●●●●●●"
+            value={password}
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+      </form>
     </div>
   );
 };
